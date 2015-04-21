7a465ab8e65418cbb59825949b1a400c3c711fe4
YulianaB <yulikristy@gmail.com>
2015-04-21 12:21:11 -0500

    02. Post con ng-repeat

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -5,4 +5,11 @@ var modulo1 =
 		'$scope', 
 		function($scope) {
 			$scope.test = "Hola Angular";
+			$scope.posts = 
+			["Post 1 Hola", 
+			"Post 2", 
+			"Post 3", 
+			"Post 4",
+			"Post 5",
+			"Post 6"];
 		}]);
