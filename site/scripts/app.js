8271dbdad2c151066e3b708bda29fa97db10c7fe
YulianaB <yulikristy@gmail.com>
2015-05-07 12:59:21 -0500

    09. Se agrega un servicio del tipo factory para almacenar los posts

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,11 +1,44 @@
 console.log(" Yuli Cargo app.js");
 var modulo1 =
 	angular.module("redditgam", []);
+
+	//Creando un servicio del tipo factory
+
+modulo1.factory('posts', [function(){
+	//cuerpo de factory llamado post
+	var o= {
+		posts : [
+		{
+			title: "post 1", upvotes: 4,
+		comments: [
+		{author: "Julia", body:"Esta muy bien.",
+		upvotes:0},
+		{author: "Eric", body:"Es pesimo.",
+		upvotes:0}]
+	},
+
+	{
+			title: "post 2", upvotes: 4,
+		comments: [
+		{author: "Vero", body:"Me encanta.",
+		upvotes:0},
+		{author: "Ana", body:"Esto esta de hueva.",
+		upvotes:0}]
+		}
+	]
+};
+//Retornando objetos de datos persistentes
+	return o;
+}]);
+	//creando controlador
+
 	modulo1.controller("mainCtrl",[
-		'$scope', 
-		function($scope) {
+		'$scope','posts',
+		//codigo del controlador
+		function($scope, posts) {
 			$scope.test = "Hola Angular";
-			$scope.posts = 
+			//Modelo al cual se le asigna el resultado del factory
+			$scope.posts = posts.posts;
 			[ 
 			{title: "Post 1", upvotes: 5},
 			{title: "Post 2", upvotes: 10},
